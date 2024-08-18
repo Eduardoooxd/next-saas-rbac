4636483a5c57895ab07ec26687c504eb443919db
Eduardoooxd <ecouto93@gmail.com>
2024-08-18 19:31:11 +0100

    feat: added auth through github

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -2,6 +2,7 @@ import fastifyCors from '@fastify/cors'
 import fastifyJwt from '@fastify/jwt'
 import fastifySwagger from '@fastify/swagger'
 import fastifySwaggerUi from '@fastify/swagger-ui'
+import { env } from '@saas/env'
 import fastify from 'fastify'
 import {
   jsonSchemaTransform,
@@ -11,6 +12,7 @@ import {
 } from 'fastify-type-provider-zod'
 
 import { errorHandler } from './error-handler'
+import { authenticateWithGithub } from './routes/auth/authenticate-with-github'
 import { authenticateWithPassword } from './routes/auth/authenticate-with-password'
 import { createAccount } from './routes/auth/create-account'
 import { getUserProfile } from './routes/auth/get-profile'
@@ -31,7 +33,15 @@ app.register(fastifySwagger, {
       description: 'Full-stack Saas app with multi-tentant & RBAC.',
       version: '1.0.0',
     },
-    servers: [],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+      },
+    },
   },
   transform: jsonSchemaTransform,
 })
@@ -42,17 +52,18 @@ app.register(fastifySwaggerUi, {
 
 app.register(fastifyCors)
 app.register(fastifyJwt, {
-  secret: `${process.env.JWT_SECRET}`,
+  secret: env.JWT_SECRET,
 })
 
 // Routes
 app.register(createAccount)
 app.register(authenticateWithPassword)
+app.register(authenticateWithGithub)
 app.register(getUserProfile)
 app.register(requestPasswordRecover)
 app.register(resetPassword)
 
-const port = 3333
+const port = env.SERVER_PORT
 
 app.listen({ port }).then(() => {
   console.log(`HTTP Server running at port: ${port}`)
