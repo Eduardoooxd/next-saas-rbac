9a60de7a23ac60163e86876b9ee906ead58e05da
Eduardoooxd <ecouto93@gmail.com>
2024-08-18 23:22:17 +0100

    feat: added route to update organization details

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -22,6 +22,7 @@ import { createOrganization } from './routes/orgs/create-organization'
 import { getUserMembership } from './routes/orgs/get-membership'
 import { getOrganization } from './routes/orgs/get-organization'
 import { getOrganizations } from './routes/orgs/get-organizations'
+import { updateOrganizationDetails } from './routes/orgs/update-organization'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -71,6 +72,7 @@ app.register(createOrganization)
 app.register(getUserMembership)
 app.register(getOrganization)
 app.register(getOrganizations)
+app.register(updateOrganizationDetails)
 
 const port = env.SERVER_PORT
 
