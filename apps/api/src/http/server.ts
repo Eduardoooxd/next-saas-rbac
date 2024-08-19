afa97de875f0bbbd0ca4e567178f034032a45fae
Eduardoooxd <ecouto93@gmail.com>
2024-08-19 21:09:14 +0100

    feat: added route to get details of a project

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -27,6 +27,7 @@ import { transferOrganizationOwnership } from './routes/orgs/transfer-organizati
 import { updateOrganizationDetails } from './routes/orgs/update-organization'
 import { createProject } from './routes/projects/create-project'
 import { deleteProject } from './routes/projects/delete-project'
+import { getProject } from './routes/projects/get-project'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -82,6 +83,7 @@ app.register(transferOrganizationOwnership)
 
 app.register(createProject)
 app.register(deleteProject)
+app.register(getProject)
 
 const port = env.SERVER_PORT
 
