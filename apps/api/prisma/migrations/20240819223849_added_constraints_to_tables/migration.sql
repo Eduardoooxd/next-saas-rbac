08c7d3de9b9da31092fb0d8ce2a783f0c4724131
Eduardoooxd <ecouto93@gmail.com>
2024-08-19 23:39:09 +0100

    chore: added cascades to the tables

---
+++ apps/api/prisma/migrations/20240819223849_added_constraints_to_tables/migration.sql
@@ -0,0 +1,53 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `organizationId` on the `invites` table. All the data in the column will be lost.
+  - A unique constraint covering the columns `[email,organization_id]` on the table `invites` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `organization_id` to the `invites` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropForeignKey
+ALTER TABLE "accounts" DROP CONSTRAINT "accounts_user_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "invites" DROP CONSTRAINT "invites_organizationId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "members" DROP CONSTRAINT "members_organization_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "members" DROP CONSTRAINT "members_user_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "projects" DROP CONSTRAINT "projects_organization_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "tokens" DROP CONSTRAINT "tokens_user_id_fkey";
+
+-- DropIndex
+DROP INDEX "invites_email_organizationId_key";
+
+-- AlterTable
+ALTER TABLE "invites" DROP COLUMN "organizationId",
+ADD COLUMN     "organization_id" TEXT NOT NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "invites_email_organization_id_key" ON "invites"("email", "organization_id");
+
+-- AddForeignKey
+ALTER TABLE "tokens" ADD CONSTRAINT "tokens_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "accounts" ADD CONSTRAINT "accounts_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "invites" ADD CONSTRAINT "invites_organization_id_fkey" FOREIGN KEY ("organization_id") REFERENCES "organizations"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "members" ADD CONSTRAINT "members_organization_id_fkey" FOREIGN KEY ("organization_id") REFERENCES "organizations"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "members" ADD CONSTRAINT "members_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "projects" ADD CONSTRAINT "projects_organization_id_fkey" FOREIGN KEY ("organization_id") REFERENCES "organizations"("id") ON DELETE CASCADE ON UPDATE CASCADE;
