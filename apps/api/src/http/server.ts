e119dd30ea6751c13c310410b987087bd7d18892
Eduardoooxd <ecouto93@gmail.com>
2024-08-19 21:19:29 +0100

    feat: added feature to update organization project

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -29,6 +29,7 @@ import { createProject } from './routes/projects/create-project'
 import { deleteProject } from './routes/projects/delete-project'
 import { getProject } from './routes/projects/get-project'
 import { getProjects } from './routes/projects/get-projects'
+import { updateProject } from './routes/projects/update-project'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -83,6 +84,7 @@ app.register(shutdownOrganization)
 app.register(transferOrganizationOwnership)
 
 app.register(createProject)
+app.register(updateProject)
 app.register(deleteProject)
 app.register(getProject)
 app.register(getProjects)
